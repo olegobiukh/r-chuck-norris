6d8ad3a67f764fc5eeead525554300e57df6ae37
Oleg Obiukh 2020 <oleg.obiuh.subscribe@gmail.com>
2020-05-22 23:27:14 +0300

    Deploy 1

---
+++ build/precache-manifest.30647f881ee7738bc94904c811645ce0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6849da38d9afdde7477e2afff7102cd",
+    "url": "/r-chuck-norris/build/index.html"
+  },
+  {
+    "revision": "3cb209523e50ef282642",
+    "url": "/r-chuck-norris/build/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "608dd2ab76ca6acd9889",
+    "url": "/r-chuck-norris/build/static/js/2.6756850b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/r-chuck-norris/build/static/js/2.6756850b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cb209523e50ef282642",
+    "url": "/r-chuck-norris/build/static/js/main.3e73f9f1.chunk.js"
+  },
+  {
+    "revision": "1754eff16c525405b1b4",
+    "url": "/r-chuck-norris/build/static/js/runtime-main.3b15bd48.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/r-chuck-norris/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
